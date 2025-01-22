67c06decc6903b77a03affae61429b0ac7981bbe
AlexeyGerashchenko <aagerashchenko@edu.hse.ru>
2025-01-22 21:51:59 +0300

    6

--- main.go
+++ main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+
+	"UnUpset/config"
+	"UnUpset/models"
+	"UnUpset/routes"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	s := "Users"
-	fmt.Println("Hello and welcome, %s!", s)
+	config.ConnectDB()
+	err := models.Migrate(config.DB)
+	if err != nil {
+		log.Fatalf("Failed to migrate database: %v", err)
+	}
+	r := gin.Default()
+	routes.SetupRotesUsers(r)
+	routes.SetupRotesTodos(r)
+	routes.SetupRotesStatistics(r)
+	routes.SetupRotesTimer(r)
+	log.Println("Server is running on http://localhost:8080")
+	err = r.Run(":8080")
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
--- routes/users.go
+++ routes/users.go
@@ -2,13 +2,14 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"UnUpset/controllers"
 )
 
 func SetupRotesUsers (r *gin.Engine) {
 	users := r.Group("/users") {
-		router.GET("/", )
+		router.GET("/", controllers.GetAllUsers)
 		router.GET("/:id", )
-		router.POST("/", )
+		router.POST("/", controllers.CreateUser)
 		router.PATCH("/:id", )
 		router.DELETE("/:id", )
 	}
