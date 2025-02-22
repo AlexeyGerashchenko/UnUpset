ab172c47271fa57942d09d3ca496411555d9ee7a
AlexeyGerashchenko <aagerashchenko@edu.hse.ru>
2025-02-22 22:14:46 +0300

    Clean Architecture

--- models/todos.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type ToDo struct {
-	UserID uint   `gorm:"not null" json:"user_id"`
-	TaskID uint   `gorm:"primaryKey" json:"task_id"`
-	Text   string `gorm:"size:255;not null" json:"text"`
-	Status bool   `gorm:"not null" json:"status"`
-}
